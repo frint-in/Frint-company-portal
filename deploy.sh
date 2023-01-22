b926d1c9bfd07882183c068b95440b138312f5a0
Arindam <arindambhowal10@gmail.com>
2023-01-22 10:41:20 +0530

    deploy.sh file added

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+echo > .nojekyll 
+
+git init
+
+git checkout -b main
+
+git add -A 
+
+commit -m "deploy"
+
+
+git push -f git@github.com:frint-in/Frint-company-portal.git main:gh-pages
+
+cd -
